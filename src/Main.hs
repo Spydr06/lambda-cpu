4925f0985ae701c896b5635f30cb62174de3e483
Spydr06 <spydr06@web.de>
2023-10-12 00:18:25 +0200

    stdio working

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Main where
 
+import Data.Char (ord, chr)
 import Data.Primitive.ByteArray
 import Data.Map (Map)
 import qualified Data.Map
 import Control.Monad.Primitive (PrimMonad(PrimState))
-import Control.Monad (void)
+import Control.Monad
 
 data Mnemonic = Nop
     | Mov
@@ -41,8 +42,8 @@ instance Show Processor where
         where showRegister i r = "X" ++ show i ++ ":\t" ++ show r ++ "\n"
 
 class Device a where
-    readByte :: a -> Int -> Int
-    writeByte :: a -> Int -> Int
+    readByte :: a -> Int -> IO Int
+    writeByte :: a -> Int -> Int -> IO ()
 
 data GenericDevice = forall a. Device a => GenericDevice a
 
@@ -53,20 +54,30 @@ instance Device GenericDevice where
 data Memory a = Memory {
     bytes :: MutableByteArray a,
     size :: Int
-}
+} 
+
+newtype StdIO = StdIO ()
+instance Device StdIO where
+    readByte _ _ = ord <$> getChar
+    writeByte _ _ = putChar . chr 
 
 numRegisters :: Int
 numRegisters = 4
+
 newProcessor :: Processor
-newProcessor = Processor (replicate numRegisters 0) Data.Map.empty
+newProcessor = Processor (replicate numRegisters 0) $ Data.Map.fromList [
+        ((0xfffe, 0xffff), GenericDevice $ StdIO ())
+    ]
 
 main :: IO ()
 main = do
     result <- runProgram newProcessor $ Program [
             Instruction Mov [Register X0, Literal 34],
             Instruction Mov [Register X1, Literal 35],
-            Instruction Add [Register X0, Register X1]
+            Instruction Add [Register X0, Register X1],
+            Instruction Mov [Address 0xfffe, Literal 1, Register X0]
         ]
+    putChar '\n'
     print result
 
 runProgram :: Processor -> Program -> IO Processor
@@ -80,6 +91,10 @@ runInstruction :: Processor -> Instruction -> IO Processor
 -- Mov
 runInstruction cpu (Instruction Mov [Register r, Literal l]) = return $ writeRegister cpu r l
 runInstruction cpu (Instruction Mov [Register a, Register b]) = return $ writeRegister cpu a $ readRegister cpu b
+runInstruction cpu (Instruction Mov [Address a, Literal 1, Register r]) = do
+    writeByteToDevice cpu a $ readRegister cpu r
+    return cpu
+    
 runInstruction cpu (Instruction Mov _) = error "Mov: invalid instruction format."
 
 -- Add
@@ -96,3 +111,8 @@ writeRegister :: Processor -> Register -> Int -> Processor
 writeRegister cpu r v = Processor (updateRegister (registers cpu) (fromEnum r) v) $ devices cpu
     where updateRegister regs i v = take i regs ++ [v] ++ drop (i + 1) regs
 
+deviceAt :: Processor -> Int -> GenericDevice
+deviceAt cpu addr = head $ Data.Map.elems $ Data.Map.filterWithKey (\(lo, hi) _ -> addr >= lo && addr < hi) $ devices cpu
+
+writeByteToDevice :: Processor -> Int -> Int -> IO ()
+writeByteToDevice cpu addr = writeByte (deviceAt cpu addr) addr
